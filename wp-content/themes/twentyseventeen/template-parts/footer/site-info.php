cfa03a08370d8bc1d72654787dda5bca0c213c0b
vladtwett <worktwett@gmail.com>
2017-11-17 21:52:11 +0200

    Initial commit

---
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'http://www.instagram.com/vladtwett', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Powered by %s', 'twentyseventeen' ), 'twett' ); ?></a>
+</div><!-- .site-info -->
